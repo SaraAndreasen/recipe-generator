41eaff0b2dde9da28a2646836b97ae16ade18280
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-09-14 16:10:56 +0200

    Add blinking animation, change context for AI, change HTML setup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ function displayRecipe(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: null,
-    delay: 40,
+    delay: 10,
   });
 }
 
@@ -14,11 +14,11 @@ function generateRecipe(event) {
   let response = document.querySelector("#recipe-response");
   const apiKey = "c3tf38bfdaaca9c9406e786o84b5cbf8";
   let context =
-    "You are an AI assistant that loves all kinds of recipes. You like to share easy recipes with the metric system. Output the recipe using basic HTML and a line height of 1.5, you don't need to notify the user that it's written in HTML.";
+    "You are an AI assistant that loves all kinds of recipes. You like to share easy recipes with the metric system. Output the recipe using basic HTML but do not change the styling of other elements other than what you're outputting. Add a line height of 1.5 to your output, you don't need to notify the user that it's written in HTML nor display it.";
   let prompt = promptInput.value.trim();
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  response.innerHTML = "Generating recipe...";
+  response.innerHTML = `<div class="blink">🍽 Generating recipe about ${prompt}..</div>`;
 
   axios.get(apiURL).then(displayRecipe);
 }
