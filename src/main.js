0bd48195638481e4ebd9ed97c706e2c7a56fe160
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-09-14 14:50:39 +0200

    setup AI api, update styling, setup Javascript

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
-function generateRecipe(event) {
-  event.preventDefault();
+import { apiKey } from "./api.js";
 
-  let inputResult = document.querySelector("#prompt-input");
-  let recipeResponse = document.querySelector("#recipe-response");
-
+function displayRecipe(response) {
   new Typewriter("#recipe-response", {
-    strings: inputResult.value.trim(),
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 40,
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let promptInput = document.querySelector("#prompt-input");
+  let response = document.querySelector("#recipe-response");
+  let context =
+    "You are an AI assistant that loves all kinds of recipes. You like to share easy recipes with the metric system. Output the recipe using basic HTML and a line height of 1.5, you don't need to notify the user that it's written in HTML.";
+  let prompt = promptInput.value.trim();
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  response.innerHTML = "Generating recipe...";
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 const recipeGeneratorForm = document.querySelector("#recipe-generator-form");
