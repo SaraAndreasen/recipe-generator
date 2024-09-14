ec2b0d2def76a2bfdfe1ce01772c5acd9bd02bc2
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-09-14 15:05:58 +0200

    Add key

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
 
   let promptInput = document.querySelector("#prompt-input");
   let response = document.querySelector("#recipe-response");
-  const apiKey = process.env.API_KEY;
+  const apiKey = "c3tf38bfdaaca9c9406e786o84b5cbf8";
   let context =
     "You are an AI assistant that loves all kinds of recipes. You like to share easy recipes with the metric system. Output the recipe using basic HTML and a line height of 1.5, you don't need to notify the user that it's written in HTML.";
   let prompt = promptInput.value.trim();
